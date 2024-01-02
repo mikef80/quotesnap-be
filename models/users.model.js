fc9ba22e4f670fc8643322e13ac169db3158600c
Mike <mike@mike-francis.org>
2024-01-02 09:51:12 +0000

    changes ready for merging

--- models/users.model.js
+++ models/users.model.js
@@ -6,6 +6,7 @@ exports.selectUsers = async (client, mongoDbName) => {
 exports.createNewUser = async (client, mongoDbName, userInfo) => {
   await client.connect();
   const userExists = await client.db(mongoDbName).collection("Users").findOne({ username: userInfo.username });
+  
   if (userExists) return null;
   await client.db(mongoDbName).collection("Users").insertOne(userInfo);
   return client.db(mongoDbName).collection("Users").findOne({ username: userInfo.username });
